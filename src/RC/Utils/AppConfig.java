43de6456fba17091aab5a0704db150c180a3a738
eneshasani1 <75452452+eneshasani1@users.noreply.github.com>
2021-07-02 00:56:41 +0200

    Create AppConfig.java
    Class to get configuration properties

---
+++ src/RC/Utils/AppConfig.java
@@ -0,0 +1,59 @@
+package RC.Utils;
+
+import RC.models.LangEnum;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.net.URI;
+import java.util.Properties;
+
+public class AppConfig {
+    private static AppConfig instance;
+    private Properties props;
+
+    private AppConfig() {
+        try {
+            props = new Properties();
+            props.load(getClass().getResourceAsStream("../resources/config.properties"));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public static AppConfig get() {
+        if (instance == null) {
+            instance = new AppConfig();
+        }
+        return instance;
+    }
+
+    public String getAppName() {
+        return props.getProperty("appname", "PWM");
+    }
+
+    public String getVersion() {
+        return props.getProperty("version", "1.0,0");
+    }
+
+    public String getReleased() {
+        return props.getProperty("released", "2021-05-25");
+    }
+
+    public String getConnectionString() {
+        return props.getProperty("connectionString");
+    }
+
+    public String getDriverType() {
+        return props.getProperty("driverType");
+    }
+
+    public LangEnum getLanguage() {
+        return props.getProperty("lang", "en").equals("en") ? LangEnum.EN : LangEnum.AL;
+    }
+
+    public void setLanguage(LangEnum lang) throws Exception {
+        URI confPath = getClass().getResource("../resources/config.properties").toURI();
+        props.setProperty("lang", lang == LangEnum.EN ? "en" : "al");
+        props.store(new FileOutputStream(new File(confPath)), "");
+    }
+}
