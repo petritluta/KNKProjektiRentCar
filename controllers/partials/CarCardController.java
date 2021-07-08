c3015ad3a0192d956c84ce4a46ee84403d87a272
rinajasharaj <75223541+rinajasharaj@users.noreply.github.com>
2021-07-08 18:38:30 +0200

    fxml

--- controllers/partials/CarCardController.java
+++ controllers/partials/CarCardController.java
@@ -1,11 +1,18 @@
 package controllers.partials;
 
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import java.awt.*;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class BuyersCardController implements Initializable {
+public class CarCardController implements Initializable {
+
+    @FXML
+    private Label model;
+
+    @FXML
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
