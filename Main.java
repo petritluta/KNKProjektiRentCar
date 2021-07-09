a26a013551c47c50c25cf79dad07b23ab41d8a26
eneshasani1 <75452452+eneshasani1@users.noreply.github.com>
2021-07-09 03:25:39 +0200

    Update Main.java
    Added a function in main that inserts car records

--- Main.java
+++ Main.java
@@ -7,15 +7,14 @@ import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
-import models.Car;
-import models.LangEnum;
+import models.*;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import models.User;
+import repositories.CarRepo;
 
 import java.awt.event.MouseEvent;
 import java.sql.Connection;
@@ -34,33 +33,48 @@ public class Main extends Application {
     }
 
     public static void main(String[] args) throws Exception {
-         String[] brands = {"Audi","BMW","Mercedes"};
-         String[] classification = {"Sedan","SUV","Cabriolet","Sports Car"};
-        Connection conn = DbHelper.getConnection();
-        String query = "INSERT INTO car (publisher,manufacture,model,price_per_day,avg_fuel_km,transmission,speed_limit,type,seat_num,door_num,inserted_at,updated_at, car_img) VALUES (?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?, ?, ?)";
+        //insertCarRecords();
+        //launch(args);
+    }
 
-        for (int i = 0; i < brands.length; i++) {
-            for (int j = 0; j < classification.length; j++) {
-                for (int k = 1; k <= 6 ; k++) {
-                    if(k==6 && brands[i] != "BMW") continue;
-                    PreparedStatement stmt = conn.prepareStatement(query);
-                    stmt.setInt(1, 1);
-                    stmt.setInt(2, i+1);
-                    stmt.setString(3, model.getModel());
-                    stmt.setDouble(4, model.getPrice_per_day());
-                    stmt.setDouble(5, model.getAvg_fuel_km());
-                    stmt.setInt(6, model.getTransmission().getValue());
-                    stmt.setDouble(7, model.getSpeed_limit());
-                    stmt.setInt(8, model.getType().getValue());
-                    stmt.setInt(9, model.getSeat_num());
-                    stmt.setInt(10, model.getDoor_num());
-                    stmt.setTimestamp(11, java.sql.Timestamp.valueOf(java.time.LocalDateTime.now()));
-                    stmt.setTimestamp(12, java.sql.Timestamp.valueOf(DateHelper.toSqlFormat(model.getUpdated_at())));
-                    stmt.setString(13,model.getCar_img());
+    private static void insertCarRecords(){
+        try {
+            String[] brands = {"Audi", "BMW", "Mercedes"};
+            String[] classification = {"Sedan", "SUV", "Cabriolet", "Sports Car"};
+            String[] model = {"A6", "X5", "S Class"};
+            Connection conn = DbHelper.getConnection();
+            System.out.println("U konektu");
+            String query = "INSERT INTO car (publisher,manufacture,model,price_per_day,avg_fuel_km,transmission,speed_limit,type,seat_num,door_num,inserted_at,updated_at, car_img) VALUES (?, ?, ?, ?, ?,?, ?, ?, ?, ?, ?, ?, ?)";
+            String imageLocation = "resources/images/cars/";
+            for (int i = 0; i < brands.length; i++) {
+                for (int j = 0; j < classification.length; j++) {
+                    for (int k = 1; k <= 6; k++) {
+                        if (k == 6 && brands[i] != "BMW") continue;
+                        PreparedStatement stmt = conn.prepareStatement(query);
+                        stmt.setInt(1, 1);
+                        stmt.setInt(2, i + 1);
+                        stmt.setString(3, model[i]);
+                        stmt.setDouble(4, 50 + Math.random() * 20);
+                        stmt.setDouble(5, 5 + Math.random() * 5);
+                        stmt.setInt(6, Transmission.Auto.getValue());
+                        stmt.setDouble(7, 300);
+                        stmt.setInt(8, CarRepo.stringToType(classification[j]).getValue());
+                        stmt.setInt(9, 5);
+                        stmt.setInt(10, 4);
+                        stmt.setTimestamp(11, java.sql.Timestamp.valueOf(java.time.LocalDateTime.now()));
+                        stmt.setTimestamp(12, java.sql.Timestamp.valueOf(java.time.LocalDateTime.now()));
+                        stmt.setString(13, imageLocation + brands[i] + "/" + classification[j] + "/" + k + ".jpg");
+                        if (stmt.executeUpdate() != 1)
+                            throw new Exception("ERR_NO_ROW_CHANGE");
+                    }
                 }
+
             }
+        } catch (Exception ex) {
+            System.out.println("Deshtoi");
         }
-         DbHelper.getConnection();
-         launch(args);
     }
 }
+
+
+
